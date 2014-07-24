27d045a31aea13c90c8cb91cc11c6dde26cccb01
Derry Birkett <derry@d2tstudio.com>
2014-07-24 11:24:00 +0200

    Init

---
+++ harvester.js
@@ -0,0 +1,42 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to harvester. Project code is " + Session.get('projectname');;
+  };
+
+Template.hello.projects = function() {
+        var iam = Meteor.call('whoAmI', function(error, results) {
+            console.log(results.data);
+            return results.data;
+        });
+};
+
+  Template.hello.events({
+    'click input': function () {
+        var iam = Meteor.call('whoAmI', function(error, results) {
+            console.log(results);
+            Session.set('projectname',results.data[0].project.code);
+        });
+    }
+
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+        
+    Meteor.methods({
+        whoAmI: function () {
+            this.unblock();
+            return Meteor.http.call("GET", "https://d2tstudio.harvestapp.com/projects", { 
+                                    auth:     'derry@d2tstudio.com:JJqz2m3m6m.1' ,
+                                    headers: {  'Content-Type': 'application/json',
+                                                'Accept': 'application/json' }
+                            });
+        }
+    });// end methods
+
+
+
+  });
+}
